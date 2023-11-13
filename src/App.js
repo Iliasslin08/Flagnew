b63e6c604992d4c702661c170e35169a09fdde51
iliass linboui <iliasslin02@gmail.com>
2023-11-13 08:58:58 +0100

    version 205v1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {liste} from './liste.js'
+import C1 from './C1'
+import C2 from './C2.js'
+function App() {
+  return (
+    <div>
+    
+      <C2 composant={liste}/>
+    </div>
+  )
+}
+
+export default App
+
