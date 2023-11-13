3a531cd3bb736d896140ef9bcf916440c9d0a47b
iliass linboui <iliasslin02@gmail.com>
2023-11-13 11:59:11 +0100

    version master

--- src/C1.js
+++ src/C1.js
@@ -6,13 +6,14 @@ function C1(props) {
     <div className='div1'>
       <div>
         <h1>odoo</h1>
-        <img src={props.composant.photo} alt="Photo de profil" />
+      
       </div>
       <div>
         <h3>{props.composant.nom} {props.composant.prenom}</h3>
         <p>ID: {props.composant.id}</p>
         <p>Salaire: {props.composant.salaire}</p>
       </div>
+     
     </div>
   );
 }
