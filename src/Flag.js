a90bb5ea98cd35aea824336da24404a00f2d409e
iliass linboui <iliasslin02@gmail.com>
2023-11-23 13:15:07 +0100

    code source

--- src/Flag.js
+++ src/Flag.js
@@ -1,80 +1,108 @@
-import React, { useState, useEffect } from 'react';
-import './Flag.css';
+import React, { useState, useEffect } from "react";
+import "./Flag.css";
 
-function Flag() {
-  const [ALLflag, setAllFlag] = useState([]);
-  const [random, setRandom] = useState(null);
-  const [random2, setRandom2] = useState(null);
+function FlagGame() {
+  const [allFlags, setAllFlags] = useState([]);
+  const [randomFlagImage, setRandomFlagImage] = useState(null);
+  const [randomCountryName, setRandomCountryName] = useState(null);
   const [options, setOptions] = useState([]);
   const [selectedOption, setSelectedOption] = useState(null);
+  const [count, setCount] = useState(0);
   const [score, setScore] = useState({
     correct: 0,
     fals: 0,
   });
+  const [lastRandomFlagIndex, setLastRandomFlagIndex] = useState(null);
 
   useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
+    fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
-      .then((data) => setAllFlag(data))
-      .catch((error) => console.error('Error fetching data:', error));
+      .then((data) => setAllFlags(data))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
+  const totalFlags = allFlags.length;
+
   function getRandom() {
-    const randomIndex = Math.floor(Math.random() * ALLflag.length);
-    const randomFlag = ALLflag[randomIndex];
-    setRandom(randomFlag.flags.png);
-    setRandom2(randomFlag.name.common);
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * allFlags.length);
+    } while (randomIndex === lastRandomFlagIndex);
+
+    setLastRandomFlagIndex(randomIndex);
+
+    const randomFlag = allFlags[randomIndex];
+    setRandomFlagImage(randomFlag.flags.png);
+    setRandomCountryName(randomFlag.name.common);
 
     const otherOptions = [];
     while (otherOptions.length < 3) {
-      const optionIndex = Math.floor(Math.random() * ALLflag.length);
-      const optionFlag = ALLflag[optionIndex];
-      if (optionFlag.name.common !== randomFlag.name.common) {
+      const optionIndex = Math.floor(Math.random() * allFlags.length);
+      const optionFlag = allFlags[optionIndex];
+      if (
+        optionFlag.name.common !== randomFlag.name.common &&
+        !otherOptions.includes(optionFlag.name.common)
+      ) {
         otherOptions.push(optionFlag.name.common);
       }
     }
     setOptions([randomFlag.name.common, ...otherOptions]);
   }
-  function changevalue(selectedOption) {
+
+  function addCount() {
+    setCount(count + 1);
+  }
+
+  function changeValue(selectedOption) {
     setSelectedOption(selectedOption);
-    if(selectedOption===random2){
-      setScore((oldscore)=>({
-        ...oldscore,
-          correct:oldscore.correct+1
-      })) 
-    }else {
-      setScore((oldscore)=>({
-        ...oldscore,
-        fals:oldscore.fals+1
-      }))
+    if (selectedOption === randomCountryName) {
+      setScore((oldScore) => ({
+        ...oldScore,
+        correct: oldScore.correct + 1,
+      }));
+    } else {
+      setScore((oldScore) => ({
+        ...oldScore,
+        fals: oldScore.fals + 1,
+      }));
     }
+    getRandom();
+    addCount();
   }
 
   return (
     <div className="container">
-     <center><h1>games flags</h1></center> 
+      <center>
+        <h1>Flag Guessing Game</h1>
+      </center>
       <div className="center">
         <div className="score">
-          <p>BONNE REPONSE : {score.correct}</p>
-          <p>MAUVAISE REPONSE: {score.fals} </p>
+          <p>Correct Answers: {score.correct}</p>
+          <p>Incorrect Answers: {score.fals} </p>
         </div>
         <button className="button" onClick={getRandom}>
-         next one
+          play
         </button>
         <br />
-        {random && <img className="flag-image" src={random} alt="Flag" />}
+        {randomFlagImage && (
+          <img className="flag-image" src={randomFlagImage} alt="Flag" />
+        )}
         <br />
-
         <div className="options">
           {options.map((option, index) => (
-            <div key={index} className="option" onClick={() => changevalue(option)}>
+            <div
+              key={index}
+              className="option"
+              onClick={() => changeValue(option)}
+            >
               {option}
             </div>
           ))}
+          <p>{`${count}/${totalFlags}`}</p>
         </div>
       </div>
     </div>
   );
 }
 
-export default Flag;
+export default FlagGame;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import ReactDOM from 'react-dom'
-import App from './app'
+import App from './app (2)'
 
 ReactDOM.render(<App/>,document.getElementById('root'))
